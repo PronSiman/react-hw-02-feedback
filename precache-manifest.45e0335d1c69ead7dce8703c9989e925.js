149d7d56353a460fde35ddb800b92d4cda5281e6
PronSiman <Alex_vlasov@i.ua>
2020-06-21 14:52:26 +0300

    Updates

---
+++ precache-manifest.45e0335d1c69ead7dce8703c9989e925.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85e8c136ba12cf2e675f751dbe50ef1e",
+    "url": "/react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "5567ff7871daa3d65064",
+    "url": "/react-hw-02-feedback/static/js/2.2ac82ca8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-hw-02-feedback/static/js/2.2ac82ca8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51e7e17781f78a500828",
+    "url": "/react-hw-02-feedback/static/js/main.6877bdb1.chunk.js"
+  },
+  {
+    "revision": "388efba7dc0cc9e6ed87",
+    "url": "/react-hw-02-feedback/static/js/runtime-main.39687e55.js"
+  }
+]);
